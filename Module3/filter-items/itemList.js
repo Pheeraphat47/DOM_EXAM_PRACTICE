290b8adaf467d89acaaf15d21a8ffda4211afee5
Pheeraphat Dherachaisuphakij <107838345+Pheeraphat47@users.noreply.github.com>
2023-12-06 16:41:21 +0700

    Update itemList.js

--- Module3/filter-items/itemList.js
+++ Module3/filter-items/itemList.js
@@ -1,48 +1,45 @@
-//Node.js
-const { template } = require('@babel/core')
-const products = require('./data/products.js')
-
-//Browser
-// import { products } from './data/products.js'
+import { products } from './data/products.js';
 
 function itemList(userItems) {
-  const items = userItems
+  const items = userItems;
 
   const initialPage = () => {
-    const input = document.querySelector('input')
-    input.addEventListener('input',filterItemsHandler)
-    showItems(items)
-  }
+    // ฟังก์ชันนี้เริ่มต้นหน้าเว็บโดยตั้งค่าตัวฟังก์ชันรับเหตุการณ์และแสดงรายการรายการเริ่มต้น
 
-  const filterItemsHandler = (event) => {
-    const inputValue = event.target.value.toLowerCase()
-    const filter = items.filter((product) =>
-        product.keywords.toLowerCase().includes(inputValue))
-    showItems(filter)
-  }
+    const inputElem = document.querySelector('input');
+    inputElem.addEventListener('keydown', filterItemsHandler); // เพิ่มตัวฟังก์ชันรับเหตุการณ์ไปยังฟิลด์อินพุตเพื่อจัดการการกดแป้น
+    showItems(items); // แสดงรายการรายการเริ่มต้น
+  };
+
+  const filterItemsHandler = () => {
+    // ฟังก์ชันนี้กรองรายการรายการตามอินพุตของผู้ใช้
+
+    const inputElemValue = document.querySelector('input').value.toLowerCase(); // รับค่าอินพุตและแปลงเป็นอักษรตัวพิมพ์เล็ก
+    const filterArray = items.filter((word) => word.keywords.toLowerCase().includes(inputElemValue)); // กรองรายการตามอินพุต
+    showItems(filterArray); // แสดงรายการรายการที่กรองแล้ว
+  };
 
   const showItems = (items) => {
-    const productsUl = document.querySelector('#items')
-    productsUl.textContent = ''
-    items.forEach((product) => {
-      const liElement = document.createElement('li')
-      liElement.textContent = `ID:${product.id}, NAME:${product.name}, KEYWORDS:${product.keywords}`
-      productsUl.appendChild(liElement)
-    })
-  }
+    // ฟังก์ชันนี้แสดงรายการรายการใน DOM
+
+    const ul = document.getElementById('items'); // รับองค์ประกอบรายการจาก DOM
+    ul.textContent = ''; // ล้างเนื้อหาที่มีอยู่ของรายการ
+
+    for (let index = 0; index < items.length; index++) { // วนซ้ำผ่านอาร์เรย์รายการ
+      const createLi = document.createElement('li'); // สร้างองค์ประกอบรายการรายการใหม่
+      createLi.textContent = `ID:${items[index].id}, NAME:${items[index].name}, KEYWORDS:${items[index].keywords}`; // ตั้งค่าเนื้อหาขององค์ประกอบรายการรายการ
+      ul.appendChild(createLi); // เพิ่มองค์ประกอบรายการรายการลงในรายการ
+    }
+  };
 
   return {
-    initialPage,
-    filterItemsHandler,
-    showItems
-  }
+    initialPage, // คืนค่าฟังก์ชัน initialPage
+    filterItemsHandler, // คืนค่าฟังก์ชัน filterItemsHandler
+    showItems, // คืนค่าฟังก์ชัน showItems
+  };
 }
 
-// Node.js
-module.exports = itemList
+export { itemList };
 
-//Browser
-// export { itemList }
-//
-// const { initialPage, filterItemsHandler, showItems } = itemList(products)
-// initialPage()
+const { initialPage, filterItemsHandler, showItems } = itemList(products);
+initialPage();
